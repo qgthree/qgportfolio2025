37827d3bfae6edbee4a7507b0143e2be99c14071
Quin <langstonq@gmail.com>
2025-02-17 03:39:01 -0500

    Added likes

--- src/assets/composables/dates.js
+++
@@ -1,4 +0,0 @@
-export function formatDate(process) {
-  const date = new Date(process);
-  return (date.getMonth() + 1) + '/' + date.getDate() + (new Date().getFullYear() !== date.getFullYear() ? '/' + date.getFullYear() : '');
-};
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -26,41 +26,47 @@ const getImageUrl = (assetName) => {
 <template>
   <Filters filter1="designer" filter2="developer" filter3="advisor" />
   <section id="tl_items" v-if="projects()">
-    <div class="tl_item" v-for="(project, index) in projects()" :key="index">
-      <div class="details">
-        <div class="details_left">
-          <span v-if="project.user.userImage" class="userImage" v-bind:style="{ backgroundImage: 'url(' + project.user.userImage + ')' }"></span>
-          <span v-else class="material-symbols-outlined">account_circle</span>
-          <span>{{ project.user.username }}</span>
-          <span>&nbsp;∙&nbsp;</span>
-          <span class="lighter">{{ project.year }}</span>
+    <transition-group name="tl_item" tag="div">
+      <div class="tl_item" v-for="(project) in projects()" :key="project.id">
+        <div class="details">
+          <div class="details_left">
+            <span v-if="project.user.userImage" class="userImage" v-bind:style="{ backgroundImage: 'url(' + project.user.userImage + ')' }"></span>
+            <span v-else class="material-symbols-outlined">account_circle</span>
+            <span>{{ project.user.username }}</span>
+            <span>&nbsp;∙&nbsp;</span>
+            <span class="lighter">{{ project.year }}</span>
+          </div>
+          <div class="details_right">
+          </div>
         </div>
-        <div class="details_right">
+        <h3>
+          {{ project.title }}
+        </h3>
+        <a
+          class="project_banner"
+          role="img"
+          :style="{ backgroundImage: 'url(' + getImageUrl(project.banner.image) + ')' }"
+          :aria-label="project.banner.alt"
+        >
+        </a>
+        <div class="details">
+          <div class="details_left">
+            <a v-for="(skill, index) in project.skills" class="details_button" :key="index">{{ skill }}</a>
+          </div>
+          <div class="details_right">
+            <span class="material-symbols-outlined lighter" :class="{ 'liked': project.liked }" @click="useProjectsStore().toggleProjectLike(project.id)">favorite</span>
+          </div>
         </div>
       </div>
-      <h3>
-        {{ project.title }}
-      </h3>
-      <a
-        class="project_banner"
-        role="img"
-        :style="{ backgroundImage: 'url(' + getImageUrl(project.banner.image) + ')' }"
-        :aria-label="project.banner.alt"
-      >
-      </a>
-      <div class="details">
-        <div class="details_left">
-          <a v-for="(skill, index) in project.skills" class="details_button" :key="index">{{ skill }}</a>
-        </div>
-        <div class="details_right">
-          <span class="material-symbols-outlined lighter">favorite</span>
-        </div>
-      </div>
-    </div>
+    </transition-group>
   </section>
 </template>
 
 <style scoped>
+.tl_item {
+  transition: all 0.3s;
+}
+
 .project_banner {
   display: block;
   cursor: pointer;
@@ -72,4 +78,13 @@ const getImageUrl = (assetName) => {
   transition: all 0.2s ease;
   filter: grayscale(100%);
 }
+
+.tl_item-enter, .tl_item-leave-to
+  /* .list-complete-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(-50%);
+  }
+  .tl_item-leave-active {
+    position: absolute;
+  }
 </style>
