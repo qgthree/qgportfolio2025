1f598129ff81826fc6a1c6a5e8282b308a99f90d
Quin <langstonq@gmail.com>
2025-02-17 22:16:15 -0500

    Add Project component with project listing and filtering features

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -5,41 +5,43 @@ defineProps({
 </script>
 
 <template>
-  <Filters filter1="designer" filter2="developer" filter3="advisor" />
-  <section id="tl_items">
-    <transition-group name="tl_item" tag="div">
-      <div class="tl_item" v-for="(project) in projects" :key="project.id">
-        <div class="details">
-          <div class="details_left">
-            <span v-if="project.user.userImage" class="userImage" v-bind:style="{ backgroundImage: 'url(' + project.user.userImage + ')' }"></span>
-            <span v-else class="material-symbols-outlined">account_circle</span>
-            <span>{{ project.user.username }}</span>
-            <span>&nbsp;∙&nbsp;</span>
-            <span class="lighter">{{ project.year }}</span>
+  <section class="timeline">
+    <Filters filter1="designer" filter2="developer" filter3="advisor" />
+    <div id="tl_items">
+      <transition-group name="tl_item" tag="div">
+        <div class="tl_item" v-for="(project) in projects" :key="project.id">
+          <div class="details">
+            <div class="details_left">
+              <span v-if="project.user.userImage" class="userImage" v-bind:style="{ backgroundImage: 'url(' + project.user.userImage + ')' }"></span>
+              <span v-else class="material-symbols-outlined">account_circle</span>
+              <span>{{ project.user.username }}</span>
+              <span>&nbsp;∙&nbsp;</span>
+              <span class="lighter">{{ project.year }}</span>
+            </div>
+            <div class="details_right">
+            </div>
           </div>
-          <div class="details_right">
+          <h3>
+            {{ project.title }}
+          </h3>
+          <a
+            class="project_banner"
+            role="img"
+            :style="{ backgroundImage: 'url(' + getImageUrl(project.banner.image) + ')' }"
+            :aria-label="project.banner.alt"
+          >
+          </a>
+          <div class="details">
+            <div class="details_left">
+              <a v-for="(skill, index) in project.skills" class="details_button" :key="index">{{ skill }}</a>
+            </div>
+            <div class="details_right">
+              <span class="material-symbols-outlined lighter" :class="{ 'liked': project.liked }" @click="useProjectsStore().toggleProjectLike(project.id)">favorite</span>
+            </div>
           </div>
         </div>
-        <h3>
-          {{ project.title }}
-        </h3>
-        <a
-          class="project_banner"
-          role="img"
-          :style="{ backgroundImage: 'url(' + getImageUrl(project.banner.image) + ')' }"
-          :aria-label="project.banner.alt"
-        >
-        </a>
-        <div class="details">
-          <div class="details_left">
-            <a v-for="(skill, index) in project.skills" class="details_button" :key="index">{{ skill }}</a>
-          </div>
-          <div class="details_right">
-            <span class="material-symbols-outlined lighter" :class="{ 'liked': project.liked }" @click="useProjectsStore().toggleProjectLike(project.id)">favorite</span>
-          </div>
-        </div>
-      </div>
-    </transition-group>
+      </transition-group>
+    </div>
   </section>
 </template>
 
